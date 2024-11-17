7f14f35036deaf8a95017129003148e39763f48e
OmgRod <omgrod1000@proton.me>
2024-11-17 10:00:47 +0000

    default site

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' folder
+app.use(express.static('public'));
+app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
+app.use('/bootstrap-icons', express.static('node_modules/bootstrap-icons/'));
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.render('index'); // Renders index.ejs
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
